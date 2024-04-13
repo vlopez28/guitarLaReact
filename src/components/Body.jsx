9fd7da1501f51075bed8174e7569a290cc32c15b
Victoria Lopez <vlopez@alumnos.exa.unicen.edu.ar>
2024-04-13 16:04:23 -0300

    iniciando proyecto react, GuitarLa

---
+++ src/components/Body.jsx
@@ -0,0 +1,23 @@
+import Guitar from "./Guitar"
+
+export default function Body  () {
+  return (
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+            <Guitar/>
+            <Guitar/>
+            <Guitar/>
+            <Guitar/>
+            <Guitar/>
+            <Guitar/>
+            <Guitar/>
+            <Guitar/>
+            <Guitar/>
+        </div>
+    </main>
+  )
+}
+
+ Body
