891a0ff19eeb5bdd9e22a65e02ea3bfa2605d391
Victoria Lopez <vlopez@alumnos.exa.unicen.edu.ar>
2024-04-14 01:14:04 -0300

    agrego estado en el body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,20 +1,27 @@
 import Guitar from "./Guitar"
+import { useState } from "react"
+import { db } from '../data/db.js'
 
 export default function Body  () {
+  const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+
+
+
   return (
     <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
+            {data.map((guitar, id) =>(
+              <Guitar
+                key={id}
+                guitar = {guitar}
+                setCart = {setCart}
+              />
+            ))}
+            
+            
         </div>
     </main>
   )
